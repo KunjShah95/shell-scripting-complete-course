99319abbc9391d71422d92453616272a11d699d5
KunjShah95 <kkshah05072005@gmail.com>
2025-04-08 17:34:27 +0530

    first commit

---
+++ COMMENTS/comments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Checking Comments"
+# This is a single-line comment
+
+
+: <<comment 
+This is a multi-line comment
+that spans multiple lines
+comment
---
+++ variables/variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Script to how to use variables in bash
+
+a=10
+name="Kunj"
+echo "The value of a is $a"
+echo "The value of name is $name"
+read name
+echo "The value of name is $name"
